4151a9e60032a5ed971c869e07ae34cdbd043dd6
vityasyyy <argyavityasy1208@gmail.com>
2025-02-07 00:06:49 +0700

    feat(auth): added password hash and fixed the models a bit to match the bcrypt

--- auth-service/internal/models/user_models.go
+++ auth-service/internal/models/user_models.go
@@ -3,6 +3,6 @@ package models
 type User struct {
 	UserID      string `db:"user_id" json:"user_id"`
 	NamaUser    string `db:"nama_user" json:"nama_user" binding:"required, max=20 unique"`
-	Password    string `db:"password" json:"password" binding:"required, min=8"`
+	Password    string `db:"password" json:"password" binding:"required, min=8 max=72"`
 	AsalSekolah string `db:"asal_sekolah" json:"asal_sekolah" binding:"required"`
 }
--- auth-service/internal/utils/pw_hash.go
+++ auth-service/internal/utils/pw_hash.go
@@ -1,1 +1,15 @@
 package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	// hashes the password using bcrypt
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	// compares the hashed password in the database with the password that the user entered
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
