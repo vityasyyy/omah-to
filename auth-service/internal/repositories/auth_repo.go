074adc0d032b69748f1c3eb81df35820998e3901
vityasyyy <argyavityasy1208@gmail.com>
2025-02-06 23:46:42 +0700

    feat(auth): auth and token repo created

--- auth-service/internal/repositories/auth_repo.go
+++ auth-service/internal/repositories/auth_repo.go
@@ -1,1 +1,54 @@
 package repositories
+
+import (
+	"auth-service/internal/logger"
+	"auth-service/internal/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+// Must implement all methods in the interface
+type AuthRepo interface {
+	CreateUser(user *models.User) error
+	GetUserByUsername(username string) (*models.User, error)
+}
+
+type authRepo struct {
+	db *sqlx.DB // Depends on a database connection
+}
+
+// Constructor for authRepo that returns the interface
+func NewAuthRepo(db *sqlx.DB) AuthRepo {
+	// return the pointer to a struct with the database connection
+	return &authRepo{db: db}
+}
+
+// This function implements the CreateUser method from the AuthRepo interface, it creates a new user in the database, accepts the user models as the params, and returns an error if the query fails
+func (r *authRepo) CreateUser(user *models.User) error {
+	// Define the query to insert a new user
+	query := "INSERT INTO users (nama_user, password, asal_sekolah) VALUES ($1, $2, $3) RETURNING user_id"
+	// Execute the query and scan the result into the user struct
+	err := r.db.QueryRow(query, user.UserID, user.NamaUser, user.Password, user.AsalSekolah).Scan(&user.UserID)
+	if err != nil {
+		// Log the error if the query fails
+		logger.LogError(err, "Failed to create user", map[string]interface{}{"layer": "repository", "operation": "CreateUser"})
+		return err
+	}
+	// Log the success if the query succeeds (this wont be logged in production)
+	logger.LogDebug("User created", map[string]interface{}{"layer": "repository", "operation": "CreateUser"})
+	return nil
+}
+
+func (r *authRepo) GetUserByUsername(username string) (*models.User, error) {
+	// Create a new user struct to store the result
+	var user models.User
+	query := "SELECT user_id, nama_user, asal_sekolah FROM users WHERE nama_user = $1"
+	// Get the user struct from the database using the query and the username
+	err := r.db.Get(&user, query, username)
+	if err != nil {
+		logger.LogError(err, "Failed to get user", map[string]interface{}{"layer": "repository", "operation": "GetUserByUsername"})
+		return nil, err
+	}
+	logger.LogDebug("User retrieved", map[string]interface{}{"layer": "repository", "operation": "GetUserByUsername"})
+	return &user, nil
+}
---
+++ auth-service/internal/repositories/refresh_token_repo.go
@@ -0,0 +1,56 @@
+package repositories
+
+import (
+	"auth-service/internal/logger"
+	"auth-service/internal/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type RefreshTokenRepo interface {
+	StoreRefreshToken(refreshTokenStruct *models.RefreshToken) error
+	RevokeRefreshToken(refreshTokenString string) error
+	FindValidRefreshToken(refreshTokenString string) (*models.RefreshToken, error)
+}
+
+type refreshTokenRepo struct {
+	db *sqlx.DB
+}
+
+func NewTokenRepository(db *sqlx.DB) RefreshTokenRepo {
+	return &refreshTokenRepo{db: db}
+}
+
+func (r *refreshTokenRepo) StoreRefreshToken(refreshTokenStruct *models.RefreshToken) error {
+	query := "INSERT INTO refresh_tokens (user_id, refresh_token_value, expired_at, created_at, revoked) VALUES ($1, $2, $3, $4, $5)"
+	_, err := r.db.Exec(query, refreshTokenStruct.UserID, refreshTokenStruct.RefreshTokenValue, refreshTokenStruct.ExpiredAt, refreshTokenStruct.CreatedAt, refreshTokenStruct.Revoked)
+	if err != nil {
+		logger.LogError(err, "Failed to store refresh token", map[string]interface{}{"layer": "repository", "operation": "StoreRefreshToken"})
+		return err
+	}
+	logger.LogDebug("Refresh token stored", map[string]interface{}{"layer": "repository", "operation": "StoreRefreshToken"})
+	return nil
+}
+
+func (r *refreshTokenRepo) RevokeRefreshToken(refreshTokenString string) error {
+	query := "UPDATE refresh_tokens SET revoked = true WHERE refresh_token_value = $1 AND revoked = false AND expired_at > CURRENT_TIMESTAMP"
+	_, err := r.db.Exec(query, refreshTokenString)
+	if err != nil {
+		logger.LogError(err, "Failed to revoke refresh token", map[string]interface{}{"layer": "repository", "operation": "RevokeRefreshToken"})
+		return err
+	}
+	logger.LogDebug("Refresh token revoked", map[string]interface{}{"layer": "repository", "operation": "RevokeRefreshToken"})
+	return nil
+}
+
+func (r *refreshTokenRepo) FindValidRefreshToken(refreshTokenString string) (*models.RefreshToken, error) {
+	var refreshToken models.RefreshToken
+	query := "SELECT * FROM refresh_tokens WHERE refresh_token_value = $1 AND revoked = false AND expired_at > CURRENT_TIMESTAMP"
+	err := r.db.Get(&refreshToken, query, refreshTokenString)
+	if err != nil {
+		logger.LogError(err, "Failed to find valid refresh token", map[string]interface{}{"layer": "repository", "operation": "FindValidRefreshToken"})
+		return nil, err
+	}
+	logger.LogDebug("Refresh token found", map[string]interface{}{"layer": "repository", "operation": "FindValidRefreshToken"})
+	return &refreshToken, nil
+}
