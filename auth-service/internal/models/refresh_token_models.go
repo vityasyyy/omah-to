665e72746516a7c881a32576e904f1bb707a10f2
vityasyyy <argyavityasy1208@gmail.com>
2025-02-07 13:44:25 +0700

    fix(auth/models): changed id to int instead of string

--- auth-service/internal/models/refresh_token_models.go
+++ auth-service/internal/models/refresh_token_models.go
@@ -1,10 +1,12 @@
 package models
 
+import "time"
+
 type RefreshToken struct {
-	RefreshTokenID    string `db:"refresh_token_id" json:"refresh_token_id"`
-	UserID            string `db:"user_id" json:"user_id"`
-	RefreshTokenValue string `db:"refresh_token_value" json:"refresh_token_value" binding:"required"`
-	ExpiredAt         string `db:"expired_at" json:"expired_at" binding:"required"`
-	CreatedAt         string `db:"created_at" json:"created_at" binding:"required" default:"CURRENT_TIMESTAMP"`
-	Revoked           bool   `db:"revoked" json:"revoked" binding:"required" default:"false"`
+	RefreshTokenID    int       `db:"refresh_token_id" json:"refresh_token_id"`
+	UserID            int       `db:"user_id" json:"user_id"`
+	RefreshTokenValue string    `db:"refresh_token_value" json:"refresh_token_value" binding:"required"`
+	ExpiredAt         time.Time `db:"expired_at" json:"expired_at" binding:"required"`
+	CreatedAt         time.Time `db:"created_at" json:"created_at" binding:"required" default:"CURRENT_TIMESTAMP"`
+	Revoked           bool      `db:"revoked" json:"revoked" binding:"required" default:"false"`
 }
