7d555142ecba633f1a78716f34bfd6c912509400
vityasyyy <argyavityasy1208@gmail.com>
2025-02-08 09:15:27 +0700

    feat(auth/routes): added routes for our handlers

--- auth-service/internal/routes/auth_routes.go
+++ auth-service/internal/routes/auth_routes.go
@@ -1,1 +1,24 @@
 package routes
+
+import (
+	"auth-service/internal/handlers"
+	"auth-service/internal/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitializeRoutes(r *gin.Engine, userHandler *handlers.UserHandler) {
+	public := r.Group("/user")
+	{
+		public.POST("/register", userHandler.RegisterUserHandler)
+		public.POST("/login", userHandler.LoginUserHandler)
+		public.POST("/refresh", userHandler.RefreshTokenHandler)
+	}
+
+	authorized := r.Group("/auth")
+	authorized.Use(utils.ValidateAccessTokenMiddleware())
+	{
+		authorized.GET("/validateprofile", userHandler.ValidateUserAndGetInfoHandler)
+		authorized.POST("/logout", userHandler.LogoutUserHandler)
+	}
+}
