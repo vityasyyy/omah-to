10ae089e4af6ba52c31238a0915edc924a6443f3
vityasyyy <argyavityasy1208@gmail.com>
2025-02-08 09:15:49 +0700

    feat(auth/): added main go for entrypoint of the app

--- auth-service/main.go
+++ auth-service/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"auth-service/internal/handlers"
+	"auth-service/internal/logger"
+	"auth-service/internal/repositories"
+	"auth-service/internal/routes"
+	"auth-service/internal/services"
+	"auth-service/internal/utils"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	logger.InitLogger()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sqlx.Connect("postgres", dbURL)
+	if err != nil {
+		logger.Log.Fatal().Err(err).Msg("Failed to connect to the database")
+	}
+	defer db.Close()
+
+	authRepo := repositories.NewAuthRepo(db)
+	refreshTokenRepo := repositories.NewTokenRepository(db)
+
+	tokenService := services.NewRefreshTokenService(refreshTokenRepo, authRepo)
+	authService := services.NewAuthService(authRepo, tokenService)
+
+	userHandler := handlers.NewUserHandler(authService, tokenService)
+
+	r := gin.Default()
+	r.Use(utils.ReqLoggingMiddleware())
+	routes.InitializeRoutes(r, userHandler)
+
+	if err := r.Run(os.Getenv("PORT")); err != nil {
+		logger.Log.Fatal().Err(err).Msg("Failed to start the server")
+	}
+
+	logger.Log.Info().Msg("Server started successfully")
+}
