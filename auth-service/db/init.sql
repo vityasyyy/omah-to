a1b562648cd1adccbe7d6791bdb5e02540f93d7a
vityasyyy <argyavityasy1208@gmail.com>
2025-02-08 23:50:39 +0700

    fix(auth/db/init.sql): fixed db schema so that username is unique and password length is 255 type shit

--- auth-service/db/init.sql
+++ auth-service/db/init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(
     user_id SERIAL PRIMARY KEY,
-    nama_user VARCHAR(255) NOT NULL,
-    password VARCHAR(72) NOT NUll,
+    nama_user VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NUll,
     asal_sekolah VARCHAR(255) NOT NULL
 );
 
