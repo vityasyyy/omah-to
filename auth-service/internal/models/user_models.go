97e2f292b6ef848c879b728673d78530980a8b05
vityasyyy <argyavityasy1208@gmail.com>
2025-02-10 09:10:16 +0700

    feat(auth/models/user_models.go): added reset token and reset token expiry to user models

--- auth-service/internal/models/user_models.go
+++ auth-service/internal/models/user_models.go
@@ -1,9 +1,13 @@
 package models
 
+import "time"
+
 type User struct {
-	UserID      int    `db:"user_id" json:"user_id"`
-	Email       string `db:"email" json:"email" binding:"required,email"`
-	NamaUser    string `db:"nama_user" json:"nama_user" binding:"required,max=20"`
-	Password    string `db:"password" json:"password" binding:"required,min=8"`
-	AsalSekolah string `db:"asal_sekolah" json:"asal_sekolah" binding:"required"`
+	UserID              int       `db:"user_id" json:"user_id"`
+	Email               string    `db:"email" json:"email" binding:"required,email"`
+	NamaUser            string    `db:"nama_user" json:"nama_user" binding:"required,max=20"`
+	AsalSekolah         string    `db:"asal_sekolah" json:"asal_sekolah" binding:"required"`
+	Password            string    `db:"password" json:"password" binding:"required,min=8"`
+	ResetToken          string    `db:"reset_token" json:"reset_token"`
+	ResetTokenExpiredAt time.Time `db:"reset_token_expired_at" json:"reset_token_expired_at"`
 }
