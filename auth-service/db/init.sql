45475cc59111cf48db928fd8c1e97a894b1acddd
vityasyyy <argyavityasy1208@gmail.com>
2025-02-10 09:08:48 +0700

    migration(auth/db/init.sql): migration schema changed, reset token now in schemea

--- auth-service/db/init.sql
+++ auth-service/db/init.sql
@@ -2,8 +2,10 @@ CREATE TABLE IF NOT EXISTS users(
     user_id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     nama_user VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NUll,
-    asal_sekolah VARCHAR(255) NOT NULL
+    asal_sekolah VARCHAR(255) NOT NULL,
+    reset_token VARCHAR(255),
+    reset_token_expired_at TIMESTAMP,
+    password VARCHAR(255) NOT NUll
 );
 
 CREATE TABLE IF NOT EXISTS refresh_tokens (
