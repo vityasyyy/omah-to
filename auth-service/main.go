4d93a1e956d7ded5a2d6e98f94c1aa1501754d01
vityasyyy <argyavityasy1208@gmail.com>
2025-02-11 16:12:37 +0700

    feat(auth/): changed jwt signing algorithm and added middlewares for main go for usual security

--- auth-service/main.go
+++ auth-service/main.go
@@ -8,10 +8,14 @@ import (
 	"auth-service/internal/services"
 	"auth-service/internal/utils"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"github.com/ulule/limiter/v3"
+	memory "github.com/ulule/limiter/v3/drivers/store/memory"
 )
 
 func main() {
@@ -32,12 +36,61 @@ func main() {
 	userHandler := handlers.NewUserHandler(authService, tokenService)
 
 	r := gin.Default()
-	r.Use(utils.ReqLoggingMiddleware())
-	routes.InitializeRoutes(r, userHandler)
+	r.Use(utils.ReqLoggingMiddleware()) // Request logging middleware
+	r.Use(securityHeadersMiddleware())  // Security headers middleware
 
-	if err := r.Run(os.Getenv("PORT")); err != nil {
+	// CORS middleware
+	r.Use(cors.New(cors.Config{
+		//AllowOrigins:     []string{"http://localhost:3000", "https://auth.omahtryout.web.id", "http://tryout-service-api:8082", "h"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length", "Authorization", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	r.Use(rateLimiterMiddleware()) // Rate limiter middleware
+
+	routes.InitializeRoutes(r, userHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := r.Run(":" + port); err != nil {
 		logger.Log.Fatal().Err(err).Msg("Failed to start the server")
 	}
 
 	logger.Log.Info().Msg("Server started successfully")
+}
+
+// securityHeadersMiddleware adds security headers to all responses
+func securityHeadersMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("X-Content-Type-Options", "nosniff")
+		c.Writer.Header().Set("X-Frame-Options", "DENY")
+		c.Writer.Header().Set("X-XSS-Protection", "1; mode=block")
+		// c.Writer.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload") // Enforce HTTPS
+		c.Writer.Header().Set("Content-Security-Policy", "default-src 'self'; frame-ancestors 'none';") // Prevents XSS
+		c.Next()
+	}
+}
+
+func rateLimiterMiddleware() gin.HandlerFunc {
+	store := memory.NewStore()
+	rate := limiter.Rate{
+		Period: 1 * time.Minute,
+		Limit:  25, // Allow max 10 requests per minute per IP
+	}
+	// Create a new rate limiter instance
+	instance := limiter.New(store, rate)
+
+	return func(c *gin.Context) {
+		ctx, err := instance.Get(c, c.ClientIP())
+		// If an error occurred or the limit is reached, abort the request
+		if err != nil || ctx.Reached {
+			c.AbortWithStatusJSON(429, gin.H{"error": "Too many requests"})
+			return
+		}
+		c.Next()
+	}
 }
