83e892b75475ef558e94e9d35c0b71d631f02c32
vityasyyy <argyavityasy1208@gmail.com>
2025-02-15 14:16:55 +0700

    migration(soal): fixed table structure and typo

--- soal-service/db/init.sql
+++ soal-service/db/init.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS pilihan_pilihan_ganda (
 CREATE TABLE IF NOT EXISTS pilihan_true_false (
     pilihan_true_false_id VARCHAR(36) PRIMARY KEY,
     kode_soal VARCHAR(36) NOT NULL,
-    pilihan_tf TEXT NOT NULL
+    pilihan_tf TEXT NOT NULL,
     jawaban BOOLEAN NOT NULL,
     FOREIGN KEY (kode_soal) REFERENCES soal(kode_soal) ON DELETE CASCADE
 );
