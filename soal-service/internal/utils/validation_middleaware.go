eb3ea00b2a838e687bdd1c15ca443494854beec2
vityasyyy <argyavityasy1208@gmail.com>
2025-02-19 14:45:19 +0700

    fix(soal/utils): fixed validation logic to check for both access and tryout token

--- soal-service/internal/utils/validation_middleaware.go
+++ soal-service/internal/utils/validation_middleaware.go
@@ -10,28 +10,42 @@ import (
 
 func ValidateToAuthApi() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tryoutToken, err := c.Cookie("tryout_token")
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Failed to get tryout token"})
-			c.Abort()
-			return
-		}
-		if tryoutToken == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Tryout token is required"})
+		// get the tryout token or access token from the context cookie sent by the client
+		tryoutToken, errTryout := c.Cookie("tryout_token")
+		accessToken, errAccess := c.Cookie("access_token")
+
+		var authServiceURL string
+		var token string
+
+		// check if the tryout token or access token is present
+		if errTryout == nil && tryoutToken != "" {
+			authServiceURL = os.Getenv("AUTH_SERVICE_URL") + "/tryout/validatetryout"
+			token = tryoutToken
+		} else if errAccess == nil && accessToken != "" {
+			authServiceURL = os.Getenv("AUTH_SERVICE_URL") + "/auth/validateprofile"
+			token = accessToken
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No valid authentication token found"})
 			c.Abort()
 			return
 		}
 
-		authServiceURL := os.Getenv("AUTH_SERVICE_URL") + "/auth/tryoutvalidate"
-
+		// send a request to the auth service to validate the token
 		req, err := http.NewRequest("GET", authServiceURL, nil)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create request"})
 			c.Abort()
 			return
 		}
-		req.Header.Add("Cookie", fmt.Sprintf("tryout_token=%s", tryoutToken))
 
+		// add a cookie to the request
+		if tryoutToken != "" {
+			req.Header.Add("Cookie", fmt.Sprintf("tryout_token=%s", token))
+		} else {
+			req.Header.Add("Cookie", fmt.Sprintf("access_token=%s", token))
+		}
+
+		// make a http client, send the request, and check the response
 		client := &http.Client{}
 		response, err := client.Do(req)
 		if err != nil {
@@ -41,7 +55,7 @@ func ValidateToAuthApi() gin.HandlerFunc {
 		}
 		defer response.Body.Close()
 		if response.StatusCode != http.StatusOK {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid tryout token"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authentication token"})
 			c.Abort()
 			return
 		}
