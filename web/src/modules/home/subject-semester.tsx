f3e1c867f1241e2cfc99808040b54f2f25c5eea2
Ayasha Rahmadinni <ayashar@Ayashas.local>
2025-02-20 13:43:53 +0700

    edited semester button function

--- web/src/modules/home/subject-semester.tsx
+++ web/src/modules/home/subject-semester.tsx
@@ -1,170 +1,172 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 
-
 const BUTTON_CONTENT = [
-    {
-      label: "Semester X",
-      subjects: [
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      ],
-    },
-    {
-        label: "Semester X",
-        subjects: [
-          { code: "0X", subject: "Mata Kuliah", sks: "X" },
-          { code: "0X", subject: "Mata Kuliah", sks: "X" },
-          { code: "0X", subject: "Mata Kuliah", sks: "X" },
-          { code: "0X", subject: "Mata Kuliah", sks: "X" },
-          { code: "0X", subject: "Mata Kuliah", sks: "X" },
-          { code: "0X", subject: "Mata Kuliah", sks: "X" },
-          { code: "0X", subject: "Mata Kuliah", sks: "X" },
-          { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        ],
-      },
-    {
-      label: "Semester X",
-      subjects: [
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      ],
-    },
-    {
-      label: "Semester X",
-      subjects: [
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      ],
-    },
-    {
-      label: "Semester X",
-      subjects: [
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      ],
-    },
-    {
-      label: "Semester X",
-      subjects: [
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      ],
-    },
-    {
-      label: "Semester X",
-      subjects: [
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      ],
-    },
-    {
-      label: "Semester X",
-      subjects: [
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      ],
-    },
-    {
-      label: "Semester X",
-      subjects: [
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      ],
-    },
-    {
-      label: "Semester X" ,
-      subjects: [
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-        { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      ],
-    },
-  ];
-  
+  {
+    label: "Semester X",
+    subjects: [
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+  {
+    label: "Semester X",
+    subjects: [
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+  {
+    label: "Semester X",
+    subjects: [
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+  {
+    label: "Semester X",
+    subjects: [
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+  {
+    label: "Semester X",
+    subjects: [
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+  {
+    label: "Semester X",
+    subjects: [
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+  {
+    label: "Semester X",
+    subjects: [
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+  {
+    label: "Semester X",
+    subjects: [
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+  {
+    label: "Semester X",
+    subjects: [
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+  {
+    label: "Semester X",
+    subjects: [
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+];
 
-  const SubjectSemester = () => {
-    useEffect(() => {
-      const link = document.createElement("link");
-      link.rel = "stylesheet";
-      link.href =
-        "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200";
-      document.head.appendChild(link);
-    }, []);
-  
-    const [openSemester, setOpenSemester] = useState<number | null>(null);
-  
-    const toggleSemester = (index: number) => {
-      setOpenSemester(openSemester === index ? null : index);
-    };
-  
-    return (
-      <div className="flex w-full flex-col py-8 px-6 text-right text-align-left mx-auto">
-            <section className="flex flex-col gap-6">
-                <h1 className="text-2xl font-bold lg:text-3xl">
-                    Mata Kuliah yang Akan Membentuk Masa Depanmu di
-                    <span className="text-primary-500"> Computer Science</span>!
-                </h1>
-                <h3 className="text-xl font-bold">
-                    Universitas Gadjah Mada
-                </h3>
+const SubjectSemester = () => {
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href =
+      "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200";
+    document.head.appendChild(link);
+  }, []);
 
-          {/* Semester Buttons */}
-          <div className="grid grid-cols-2 sm:grid-cols-2 gap-6">
-            {BUTTON_CONTENT.map((item, index) => (
+  const [openSemesters, setOpenSemesters] = useState<number[]>([]);
+
+  const toggleSemester = (index: number) => {
+    setOpenSemesters((prevOpen) =>
+      prevOpen.includes(index)
+        ? prevOpen.filter((i) => i !== index)
+        : [...prevOpen, index]
+    );
+  };
+
+  return (
+    <div className="flex w-full flex-col py-8 px-6 text-right mx-auto">
+      <section className="flex flex-col gap-6">
+        <h1 className="text-2xl font-bold lg:text-3xl">
+          Mata Kuliah yang Akan Membentuk Masa Depanmu di
+          <span className="text-primary-500"> Computer Science</span>!
+        </h1>
+        <h3 className="text-xl font-bold">Universitas Gadjah Mada</h3>
+
+        {/* Semester Buttons */}
+        <div className="grid grid-cols-2 sm:grid-cols-2 gap-6">
+          {BUTTON_CONTENT.map((item, index) => {
+            const isOpen = openSemesters.includes(index);
+            return (
               <div key={index} className="w-full">
                 <button
                   onClick={() => toggleSemester(index)}
@@ -172,14 +174,14 @@ const BUTTON_CONTENT = [
                 >
                   <span className="text-lg">{item.label}</span>
                   <span className="material-symbols-outlined text-xl">
-                    {openSemester === index ? "keyboard_arrow_up" : "keyboard_arrow_down"}
+                    {isOpen ? "keyboard_arrow_up" : "keyboard_arrow_down"}
                   </span>
                 </button>
 
                 {/* Subject List */}
                 <div
                   className={`overflow-hidden shadow-md border-[#304A91] transition-all duration-700 ease-in-out ${
-                    openSemester === index ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+                    isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
                   }`}
                 >
                   <div className="rounded-md p-4 mt-2 border-[#304A91] border-2 shadow-md">
@@ -187,16 +189,27 @@ const BUTTON_CONTENT = [
                       <thead className="border-b-2 border-b-[#304A91] text-[#304A91]">
                         <tr>
                           <th className="w-[10%] px-4 py-3 text-center">No</th>
-                          <th className="w-[60%] px-4 py-3 text-center">Mata Kuliah</th>
+                          <th className="w-[60%] px-4 py-3 text-center">
+                            Mata Kuliah
+                          </th>
                           <th className="w-[30%] px-4 py-3 text-center">SKS</th>
                         </tr>
                       </thead>
                       <tbody>
                         {item.subjects.map((subject, idx) => (
-                          <tr key={idx} className="hover:bg-gray-200 text-[#304A91] transition">
-                            <td className="px-4 py-3 text-center">{subject.code}</td>
-                            <td className="px-4 py-3 text-center">{subject.subject}</td>
-                            <td className="px-4 py-3 text-center">{subject.sks}</td>
+                          <tr
+                            key={idx}
+                            className="hover:bg-gray-200 text-[#304A91] transition"
+                          >
+                            <td className="px-4 py-3 text-center">
+                              {subject.code}
+                            </td>
+                            <td className="px-4 py-3 text-center">
+                              {subject.subject}
+                            </td>
+                            <td className="px-4 py-3 text-center">
+                              {subject.sks}
+                            </td>
                           </tr>
                         ))}
                       </tbody>
@@ -204,11 +217,12 @@ const BUTTON_CONTENT = [
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
-        </section>
+            );
+          })}
         </div>
-    );
-  };
-  
+      </section>
+    </div>
+  );
+};
+
 export default SubjectSemester;
