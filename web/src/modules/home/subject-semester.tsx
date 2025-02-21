5093c98e58173f7f53f17007d6be1a29d8e7cc6b
Ayasha Rahmadinni <ayashar@Ayashas.local>
2025-02-21 09:03:08 +0700

    fix(web/landing): fix SubjectSemester animation

--- web/src/modules/home/subject-semester.tsx
+++ web/src/modules/home/subject-semester.tsx
@@ -1,135 +1,138 @@
 "use client";
 import { useEffect, useState } from "react";
 
-const BUTTON_CONTENT = [
+const BUTTON_CONTENT_LEFT = [
   {
     label: "Semester X",
     subjects: [
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
     ],
   },
   {
     label: "Semester X",
     subjects: [
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
     ],
   },
   {
     label: "Semester X",
     subjects: [
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
     ],
   },
   {
     label: "Semester X",
     subjects: [
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
     ],
   },
   {
     label: "Semester X",
     subjects: [
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+    ],
+  },
+];
+
+const BUTTON_CONTENT_RIGHT = [
+  {
+    label: "Semester X",
+    subjects: [
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
     ],
   },
   {
     label: "Semester X",
     subjects: [
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
     ],
   },
   {
     label: "Semester X",
     subjects: [
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
     ],
   },
   {
     label: "Semester X",
     subjects: [
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
     ],
   },
   {
     label: "Semester X",
     subjects: [
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-    ],
-  },
-  {
-    label: "Semester X",
-    subjects: [
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
-      { code: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
+      { no: "0X", subject: "Mata Kuliah", sks: "X" },
     ],
   },
 ];
@@ -143,10 +146,20 @@ const SubjectSemester = () => {
     document.head.appendChild(link);
   }, []);
 
-  const [openSemesters, setOpenSemesters] = useState<number[]>([]);
+  const [openSemestersLeft, setOpenSemestersLeft] = useState<number[]>([]);
+  const [openSemestersRight, setOpenSemestersRight] = useState<number[]>([]);
 
-  const toggleSemester = (index: number) => {
-    setOpenSemesters((prevOpen) =>
+
+  const toggleSemesterLeft = (index: number) => {
+    setOpenSemestersLeft((prevOpen) =>
+      prevOpen.includes(index)
+        ? prevOpen.filter((i) => i !== index)
+        : [...prevOpen, index]
+    );
+  };
+
+  const toggleSemesterRight = (index: number) => {
+    setOpenSemestersRight((prevOpen) =>
       prevOpen.includes(index)
         ? prevOpen.filter((i) => i !== index)
         : [...prevOpen, index]
@@ -162,61 +175,116 @@ const SubjectSemester = () => {
         </h1>
         <h3 className="text-xl font-bold">Universitas Gadjah Mada</h3>
 
-        <div className="grid grid-cols-2 sm:grid-cols-2 gap-6">ghjjhhj
-          {BUTTON_CONTENT.map((item, index) => {
-            const isOpen = openSemesters.includes(index);
-            return (
-              <div key={index} className="w-full">
-                <button
-                  onClick={() => toggleSemester(index)}
-                  className="ease-in flex items-center justify-between w-full rounded-md bg-[#304A91] text-white font-bold py-3 px-5 text-left shadow-md hover:bg-[#2b3e75] transition-all"
-                >
-                  <span className="text-lg">{item.label}</span>
-                  <span className="material-symbols-outlined text-xl">
-                    {isOpen ? "keyboard_arrow_up" : "keyboard_arrow_down"}
-                  </span>
-                </button>
+        {/* Button Container */}
+        <div className="flex flex-col items-start lg:grid lg:grid-cols-2 gap-6 mt-6 overflow-auto">
+          {/* Semester Buttons Left*/}
+          <div className="grid gap-6">
+            {BUTTON_CONTENT_LEFT.map((item, index) => {
+              const isOpen = openSemestersLeft.includes(index);
+              return(
+                <div key={index}>
+                  <button
+                    onClick={() => toggleSemesterLeft(index)}
+                    className="ease-in flex items-center justify-between w-full rounded-md bg-[#304A91] text-white font-bold py-3 px-5 text-left shadow-md hover:bg-[#2b3e75] transition-all"
+                    >
+                    <span className="text-lg">{item.label}</span>
+                    <span className="material-symbols-outlined text-xl">
+                      {isOpen ? "keyboard_arrow_up" : "keyboard_arrow_down"}
+                    </span>
+                  </button>
 
-                <div
-                  className={`overflow-hidden shadow-md border-[#304A91] transition-all duration-700 ease-in-out ${
-                    isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
-                  }`}
-                >
-                  <div className="rounded-md p-4 mt-2 border-[#304A91] border-2 shadow-md">
-                    <table className="w-full">
-                      <thead className="border-b-2 border-b-[#304A91] text-[#304A91]">
-                        <tr>
-                          <th className="w-[10%] px-4 py-3 text-center">No</th>
-                          <th className="w-[60%] px-4 py-3 text-center">
-                            Mata Kuliah
-                          </th>
-                          <th className="w-[30%] px-4 py-3 text-center">SKS</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {item.subjects.map((subject, idx) => (
-                          <tr
-                            key={idx}
-                            className="hover:bg-gray-200 text-[#304A91] transition"
-                          >
-                            <td className="px-4 py-3 text-center">
-                              {subject.code}
-                            </td>
-                            <td className="px-4 py-3 text-center">
-                              {subject.subject}
-                            </td>
-                            <td className="px-4 py-3 text-center">
-                              {subject.sks}
-                            </td>
+                  {/* Semester List Left */}
+                  <div 
+                    className={`overflow-hidden shadow-md border-[#304A91] transition-all duration-700 ease-in-out ${
+                      isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+                    }`}
+                  >
+                    <div className="rounded-md p-4 mt-2 border-[#304A91] border-2 shadow-md">
+                      <table className="w-full">
+                        <thead className="border-b-2 border-b-[#304A91] text-[#304A91]">
+                          <tr>
+                            <th className="w-[10%] px-4 py-3 text-center">No</th>
+                            <th className="w-[60%] px-4 py-3 text-center">Mata Kuliah</th>
+                            <th className="w-[30%] px-4 py-3 text-center">SKS</th>
                           </tr>
-                        ))}
-                      </tbody>
-                    </table>
+                        </thead>
+                        <tbody>
+                          {item.subjects.map((subject, idx) => (
+                            <tr key={idx}
+                              className="hover:bg-gray-200 text-[#304A91] transition">
+                              <td className="px-4 py-3 text-center">
+                                {subject.no}
+                              </td>
+                              <td className="px-4 py-3 text-center">
+                                {subject.subject}
+                              </td>
+                              <td className="px-4 py-3 text-center">
+                                {subject.sks}
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+          {/* Semester Buttons Right*/}
+          <div className="grid gap-6">
+            {BUTTON_CONTENT_RIGHT.map((item, index) => {
+              const isOpen = openSemestersRight.includes(index);
+              return(
+                <div key={index}>
+                  <button
+                    onClick={() => toggleSemesterRight(index)}
+                    className="ease-in flex items-center justify-between w-full rounded-md bg-[#304A91] text-white font-bold py-3 px-5 text-left shadow-md hover:bg-[#2b3e75] transition-all"
+                    >
+                    <span className="text-lg">{item.label}</span>
+                    <span className="material-symbols-outlined text-xl">
+                      {isOpen ? "keyboard_arrow_up" : "keyboard_arrow_down"}
+                    </span>
+                  </button>
+
+                  {/* Semester List Right */}
+                  <div 
+                    className={`overflow-hidden shadow-md border-[#304A91] transition-all duration-700 ease-in-out ${
+                      isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+                    }`}
+                  >
+                    <div className="rounded-md p-4 mt-2 border-[#304A91] border-2 shadow-md">
+                      <table className="w-full">
+                        <thead className="border-b-2 border-b-[#304A91] text-[#304A91]">
+                          <tr>
+                            <th className="w-[10%] px-4 py-3 text-center">No</th>
+                            <th className="w-[60%] px-4 py-3 text-center">Mata Kuliah</th>
+                            <th className="w-[30%] px-4 py-3 text-center">SKS</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {item.subjects.map((subject, idx) => (
+                            <tr key={idx}
+                              className="hover:bg-gray-200 text-[#304A91] transition">
+                              <td className="px-4 py-3 text-center">
+                                {subject.no}
+                              </td>
+                              <td className="px-4 py-3 text-center">
+                                {subject.subject}
+                              </td>
+                              <td className="px-4 py-3 text-center">
+                                {subject.sks}
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
     </div>
