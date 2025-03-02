0447c1cd0d92fb4574e6b4ac5fd4a51888f2da8a
vityasyyy <argyavityasy1208@gmail.com>
2025-03-02 12:42:52 +0700

    feat(minat-bakat/): main, dockerfile, db init sql, go mod and go sum

---
+++ minat-bakat-service/db/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS minat_bakat_attempt (
+    user_id INT PRIMARY KEY,
+    bakat_user VARCHAR(20) NOT NULL
+);
---
+++ minat-bakat-service/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"minat-bakat-service/internal/handlers"
+	"minat-bakat-service/internal/logger"
+	"minat-bakat-service/internal/repositories"
+	"minat-bakat-service/internal/routes"
+	"minat-bakat-service/internal/services"
+	"minat-bakat-service/internal/utils"
+	"os"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/ulule/limiter/v3"
+	memory "github.com/ulule/limiter/v3/drivers/store/memory"
+)
+
+func main() {
+	logger.InitLogger()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sqlx.Connect("postgres", dbURL)
+	if err != nil {
+		logger.Log.Fatal().Err(err).Msg("Failed to connect to the database")
+	}
+	defer db.Close()
+
+	mbRepo := repositories.NewMbRepo(db)
+	mbService := services.NewMinatBakatService(mbRepo)
+	mbHandler := handlers.NewMinatBakatHandler(mbService)
+
+	if os.Getenv("ENVIRONMENT") == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	r := gin.Default()
+
+	r.Use(utils.ReqLoggingMiddleware()) // Request logging middleware
+	r.Use(securityHeadersMiddleware())  // Security headers middleware
+
+	// CORS middleware
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // change to a specific origin later
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length", "Authorization", "Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	r.Use(rateLimiterMiddleware())             // Rate limiter middleware
+	r.Use(requestSizeLimitMiddleware(2 << 20)) // Request size limit middleware (2MB)
+	r.Use(timeoutMiddleware(20 * time.Second)) // Timeout middleware
+
+	routes.InitializeRoutes(r, mbHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8084"
+	}
+	if err := r.Run(":" + port); err != nil {
+		logger.Log.Fatal().Err(err).Msg("Failed to start the server")
+	}
+
+	logger.Log.Info().Msg("Server started successfully")
+}
+
+// securityHeadersMiddleware adds security headers to all responses
+func securityHeadersMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("X-Content-Type-Options", "nosniff")
+		c.Writer.Header().Set("X-Frame-Options", "DENY")
+		c.Writer.Header().Set("X-XSS-Protection", "1; mode=block")
+		// c.Writer.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload") // Enforce HTTPS
+		c.Writer.Header().Set("Content-Security-Policy", "default-src 'self'; frame-ancestors 'none';") // Prevents XSS
+		c.Next()
+	}
+}
+
+func rateLimiterMiddleware() gin.HandlerFunc {
+	store := memory.NewStore()
+	rate := limiter.Rate{
+		Period: 1 * time.Minute,
+		Limit:  25, // Allow max 10 requests per minute per IP
+	}
+	// Create a new rate limiter instance
+	instance := limiter.New(store, rate)
+
+	return func(c *gin.Context) {
+		ctx, err := instance.Get(c, c.ClientIP())
+		// If an error occurred or the limit is reached, abort the request
+		if err != nil || ctx.Reached {
+			c.AbortWithStatusJSON(429, gin.H{"error": "Too many requests"})
+			return
+		}
+		c.Next()
+	}
+}
+
+func timeoutMiddleware(timeout time.Duration) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(c.Request.Context(), timeout)
+		defer cancel()
+
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
+
+func requestSizeLimitMiddleware(maxSize int64) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.ContentLength > maxSize {
+			c.AbortWithStatusJSON(413, gin.H{"error": "Payload too large"})
+			return
+		}
+		c.Next()
+	}
+}
