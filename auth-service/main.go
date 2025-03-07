85236409f68ea063b81c95d6452ec9cbb406d01a
vityasyyy <argyavityasy1208@gmail.com>
2025-03-07 10:56:17 +0700

    feat(backend/): cors in main.go now accept localhost:3000

--- auth-service/main.go
+++ auth-service/main.go
@@ -45,7 +45,7 @@ func main() {
 
 	// CORS middleware
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"}, // change to a specific origin later
+		AllowOrigins:     []string{"http://localhost:3000"}, // change to a specific origin later
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length", "Authorization", "Content-Type"},
