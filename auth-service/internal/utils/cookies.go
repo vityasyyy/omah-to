ab028b47f8f632e0160ecceae9a4b87fa9baa57a
vityasyyy <argyavityasy1208@gmail.com>
2025-03-19 13:54:27 +0700

    fix(cookies): cookies domain changed

--- auth-service/internal/utils/cookies.go
+++ auth-service/internal/utils/cookies.go
@@ -1,10 +1,13 @@
 package utils
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 // later we have to have a way to set the tryout cookie so that when user is accessing the try out, the cookie doesnt expire that fast
+var cookieDomain = os.Getenv("COOKIE_DOMAIN")
 
 func SetCookie(c *gin.Context, name, value string, maxAge int, path, domain string, secure, httpOnly bool) {
 	c.SetCookie(
@@ -18,8 +21,8 @@ func SetCookie(c *gin.Context, name, value string, maxAge int, path, domain stri
 }
 
 func SetAccessAndRefresh(c *gin.Context, accessToken, refreshToken string) error {
-	SetCookie(c, "access_token", accessToken, 15*60, "/", "", true, true)        // access token, 15 mins
-	SetCookie(c, "refresh_token", refreshToken, 7*24*60*60, "/", "", true, true) // refresh token, 7 days
+	SetCookie(c, "access_token", accessToken, 15*60, "/", cookieDomain, true, true)        // access token, 15 mins
+	SetCookie(c, "refresh_token", refreshToken, 7*24*60*60, "/", cookieDomain, true, true) // refresh token, 7 days
 	return nil
 }
 
--- tryout-service/internal/utils/set_cookie.go
+++ tryout-service/internal/utils/set_cookie.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
+
+var cookieDomain = os.Getenv("COOKIE_DOMAIN")
 
 func SetCookie(c *gin.Context, name, value string, maxAge int, path, domain string, secure, httpOnly bool) {
 	c.SetCookie(
@@ -16,6 +20,6 @@ func SetCookie(c *gin.Context, name, value string, maxAge int, path, domain stri
 }
 
 func SetAccessAndRefresh(c *gin.Context, tryoutToken string) error {
-	SetCookie(c, "tryout_token", tryoutToken, 200*60, "/", "", true, true) // access token, 15 mins
+	SetCookie(c, "tryout_token", tryoutToken, 200*60, "/", cookieDomain, true, true) // access token, 15 mins
 	return nil
 }
