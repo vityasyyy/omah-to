9e5da94711588fb0a6a4c1158f2c96bfbd283307
vityasyyy <argyavityasy1208@gmail.com>
2025-03-24 14:07:02 +0700

    fix(web/eslint): ignore _ prefixed function parameter

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { "argsIgnorePattern": "^_" } // Ignore function parameters prefixed with "_"
+      ]
+    }
+  }
 ];
 
 export default eslintConfig;
