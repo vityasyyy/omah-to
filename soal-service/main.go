c340753e97bc5bdd3541bbfb5d8db5138b0e9476
vityasyyy <argyavityasy1208@gmail.com>
2025-03-25 08:42:23 +0700

    fix(fallback port):

--- soal-service/main.go
+++ soal-service/main.go
@@ -95,7 +95,7 @@ func main() {
 	// Server configuration
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8081"
+		port = "8082"
 	}
 
 	// Create a server with more controlled shutdown
--- tryout-service/main.go
+++ tryout-service/main.go
@@ -104,7 +104,7 @@ func main() {
 	// Server configuration
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8081"
+		port = "8083"
 	}
 
 	// Create a server with more controlled shutdown
