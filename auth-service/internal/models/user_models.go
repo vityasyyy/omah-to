8166906d046b6e3107214c92fb31384a8c5ddd32
Argya Vityasy <149230734+vityasyyy@users.noreply.github.com>
2025-03-28 16:43:58 +0700

    fix: nama user max 100 char

--- auth-service/internal/models/user_models.go
+++ auth-service/internal/models/user_models.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	UserID              int       `db:"user_id" json:"user_id"`
 	Email               string    `db:"email" json:"email" binding:"required,email"`
-	NamaUser            string    `db:"nama_user" json:"nama_user" binding:"required,max=20"`
+	NamaUser            string    `db:"nama_user" json:"nama_user" binding:"required,max=100"`
 	AsalSekolah         string    `db:"asal_sekolah" json:"asal_sekolah" binding:"required"`
 	Password            string    `db:"password" json:"password" binding:"required,min=8"`
 	ResetToken          string    `db:"reset_token" json:"reset_token"`
